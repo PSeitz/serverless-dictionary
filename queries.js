acb16dd40630eb48975442fff09fd701144e39ea
Pascal Seitz <pascal.seitz@gmail.com>
2017-01-24 09:21:39 +0100

    searcho

--- queries.js
+++ queries.js
@@ -1,11 +1,8 @@
 'use strict'
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT']
-var sqlite3 = require('sqlite3').verbose()
 var levenshtein = require('fast-levenshtein')
 var _ = require('lodash')
-let db = new sqlite3.Database('jmdict.sqlite', sqlite3.OPEN_READONLY)
-db = Promise.promisifyAll(db)
-const taim = require('taim')
+// const taim = require('taim')
 
 function isJapanese(string){
     return string.match(/[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]/)
@@ -194,12 +191,6 @@ class Entry {
     }
 }
 
-let KANJI_READINGS  = `SELECT kana from kanas WHERE _id in(
-        SELECT kana_id from kanjis k
-        JOIN kanji_readings r ON k._id = r.kanji_id
-        WHERE kanji=? AND ent_seq=?)`
-
-
 function addToEntry(ent_seqs, sqlquery, constructor, target, parameter1, parameter2 ){
     return db.allAsync(sqlquery).then(function(rows){
         for(let row of rows){
@@ -211,161 +202,101 @@ function addToEntry(ent_seqs, sqlquery, constructor, target, parameter1, paramet
     })
 }
 
-function getKanjis( ent_seqs) {
-    let sqlquery = "SELECT kanji as hit, ent_seq, num_occurences FROM kanjis WHERE ent_seq IN ("+ Object.keys(ent_seqs).join(",") +")"
-    return addToEntry(ent_seqs, sqlquery, Kanji, "kanjis", "num_occurences" )
-}
-
-function getKanas(ent_seqs) {
-    let sqlquery = "SELECT kana as hit, ent_seq, num_occurences, romaji FROM kanas WHERE ent_seq IN ("+ Object.keys(ent_seqs).join(",") +")"
-    return addToEntry(ent_seqs, sqlquery, Kana, "kanas", "num_occurences", "romaji" )
-}
-
-function getMeanings(ent_seqs, showGerman, showEnglish) {
-    let sqlquery = "SELECT meaning as hit, ent_seq, lang FROM meanings WHERE ent_seq IN ("+ Object.keys(ent_seqs).join(",") +")"
-    sqlquery = addLanguageFilter(sqlquery, showGerman, showEnglish)
-    return addToEntry(ent_seqs, sqlquery, Meaning, "meanings", "lang" )
-}
-
-function findEntrys(searchterm, fuzzy, printTime, showGerman, showEnglish) {
-    searchterm  = searchterm.trim()
-
-    let adjustedSearchTerm = fuzzy?'%' + searchterm +'%':searchterm
-    let startTime = process.hrtime()
-    let searchTermSQL = "'"+adjustedSearchTerm+"'"
-    let hit_entseq = {} // ent_seq : {String }
-
-    let promises = []
-
-    let method = fuzzy ? ' LIKE ' : '='
-    let KANJI_QUERY = `SELECT kanji as hit, ent_seq FROM kanjis WHERE kanji ${method}${searchTermSQL}`
-    let KANA_QUERY = `SELECT kana as hit, ent_seq FROM kanas WHERE kana ${method}${searchTermSQL}`
-    
-    let CONJUGATED_KANJI = `SELECT form as hit, ent_seq from kanjis k JOIN (SELECT kanji_id, form FROM kanji_conjugations WHERE form ${method}'${searchterm}') s ON s.kanji_id = k ._id`
-    let CONJUGATED_KANA  = `SELECT form as hit, ent_seq from kanas k JOIN (SELECT kana_id, form FROM kana_conjugations WHERE form ${method}'${searchterm}') s ON s.kana_id = k ._id`
-
-    function addQueries(queries){
-
-        queries.forEach((query) => {promises.push(getEntryIds(hit_entseq, query, searchterm, printTime))})
-    }
-
-    let searchDb = require('serverless-search').searchDb()
-    let query = {
-            search: {
-            term:'偉容',
-            path:'kanji[].text',
-            levenshtein_distance:0,
+function qpPart(searchterm, path, levenshtein_distance){
+    levenshtein_distance = levenshtein_distance || 0
+    return {
+            term: searchterm,
+            path:path,
+            levenshtein_distance:levenshtein_distance,
             firstCharExactMatch:true
         }
-    }
+}
 
-
+function getQuery(searchterm){
     if (containsKanji(searchterm)){
-        query = {
-            search: {
-                term: searchterm
-                path:'kanji[].text',
-                levenshtein_distance:0,
-                firstCharExactMatch:true
-            },
-            boost: {
-                path:'kanji[].commonness',
-                fun:Math.log,
-                param: 1
-            }
+        return {
+            search: qpPart(searchterm, 'kanji[].text'),
+            boost: { path:'kanji[].commonness', fun:Math.log, param: 1}
         }
     }else if(containsKana(searchterm)){
-        query = {
-                search: {
-                term: searchterm
-                path:'kana[].text',
-                levenshtein_distance:1,
-                firstCharExactMatch:true
-            },
-            boost: {
-                path:'kana[].commonness',
-                fun:Math.log,
-                param: 1
-            }
+        return {
+            search: qpPart(searchterm, 'kana[].text', 1),
+            boost: { path:'kana[].commonness', fun:Math.log, param: 1 }
         }
     }else {
-        query = {
-                search: {
-                term: searchterm
-                path:'kana[].text',
-                levenshtein_distance:1,
-                firstCharExactMatch:true
-            },
-            boost: {
-                path:'kana[].commonness',
-                fun:Math.log,
-                param: 1
-            }
-        }
-        
-        let ROMAJI_QUERY = function(searchterm){return `SELECT romaji as hit, ent_seq FROM kanas WHERE romaji ${method}'${searchterm}'`}
-        let MEANING_QUERY = function(searchterm){ 
-            let query = `SELECT meaning as hit, ent_seq FROM meanings WHERE meaning ${method}${searchterm} `
-            return addLanguageFilter(query, showGerman, showEnglish)
-        }
-        addQueries([ROMAJI_QUERY(searchterm), MEANING_QUERY(searchTermSQL)])
-        if (showGerman && hasUmlautLikeCharacter(searchterm.toLowerCase())){
-            let converted = convertUmlautLikeCharacter(searchterm.toLowerCase())
-            let umlautConverted = "'"+fuzzy?('%' + converted +'%'):converted+"'"
-            addQueries([ROMAJI_QUERY(umlautConverted), MEANING_QUERY(umlautConverted)])
-        }
-    }
+        // let orquery = [{search: qpPart(searchterm, 'meanings.eng[]', 1)},
+        //                {search: qpPart(searchterm, 'meanings.ger[]', 1)}]
 
-    return searchDb.searchDb('jmdict', ).then(res => {
+        let orquery = [{search: qpPart(searchterm, 'meanings.ger[]', 1)}]
+
+        if (hasUmlautLikeCharacter(searchterm.toLowerCase())){
+            let converted = convertUmlautLikeCharacter(searchterm.toLowerCase())
+            orquery.push({search: qpPart(converted, 'meanings.ger[]', 1)})
+        }
+        return { OR:orquery}
+    }
+}
+
+findEntrys('majestät', true)
+
+function findEntrys(searchterm, printTime) {
+    searchterm  = searchterm.trim()
+
+    let startTime = process.hrtime()
+
+    let searchDb = require('serverless-search').searchdb()
+
+    let query = getQuery(searchterm)
+    console.time("asdf")
+    let searchPromise =  searchDb.searchDb('jmdict', query).then(res => {
+        console.timeEnd("asdf")
         // console.log(JSON.stringify(res, null, 2))
         return res
     })
-    .should.eventually.have.length(1)
 
+    // taim('FindIds',searchPromise)
 
-    if (printTime) taim('FindIds',Promise.all(promises))
+    // return Promise.all(promises).then(() => {
+    //     return getEntries(hit_entseq, searchterm, printTime, showGerman, showEnglish)
+    // }).then((entries) => {
 
-    return Promise.all(promises).then(() => {
-        return getEntries(hit_entseq, searchterm, printTime, showGerman, showEnglish)
-    }).then((entries) => {
+    //     entries.sort(function(result1, result2) {
+    //         if (result1.getLevensthein_distance() == 0 &&  result2.getLevensthein_distance() != 0){
+    //             return -1
+    //         }else if (result1.getLevensthein_distance() != 0 &&  result2.getLevensthein_distance() == 0){
+    //             return 1
+    //         }else{
+    //             let result1Commonness = result1.getCommonness()
+    //             let result2Commonness = result2.getCommonness()
+    //             if(containsKana(searchterm) && !containsKanji(searchterm)){
+    //                 if (result1.isWrittenInKana && result1Commonness >0 && !result2.isWrittenInKana){
+    //                     return -1
+    //                 }
+    //                 if (result2.isWrittenInKana && result2Commonness >0 && !result1.isWrittenInKana){
+    //                     return 1
+    //                 }
+    //             }
+    //             if (containsKanji){
+    //                 if (result1.isContainedInFirstKanji(searchterm)) result1Commonness*=3
+    //                 if (result2.isContainedInFirstKanji(searchterm)) result2Commonness*=3
+    //             }else{
+    //                 if (result1.isFirstMeaningForLang(searchterm)) result1Commonness*=3
+    //                 if (result2.isFirstMeaningForLang(searchterm)) result2Commonness*=3
+    //             }
+    //             return result2Commonness - result1Commonness
+    //         }
+    //     })
 
-        entries.sort(function(result1, result2) {
-            if (result1.getLevensthein_distance() == 0 &&  result2.getLevensthein_distance() != 0){
-                return -1
-            }else if (result1.getLevensthein_distance() != 0 &&  result2.getLevensthein_distance() == 0){
-                return 1
-            }else{
-                let result1Commonness = result1.getCommonness()
-                let result2Commonness = result2.getCommonness()
-                if(containsKana(searchterm) && !containsKanji(searchterm)){
-                    if (result1.isWrittenInKana && result1Commonness >0 && !result2.isWrittenInKana){
-                        return -1
-                    }
-                    if (result2.isWrittenInKana && result2Commonness >0 && !result1.isWrittenInKana){
-                        return 1
-                    }
-                }
-                if (containsKanji){
-                    if (result1.isContainedInFirstKanji(searchterm)) result1Commonness*=3
-                    if (result2.isContainedInFirstKanji(searchterm)) result2Commonness*=3
-                }else{
-                    if (result1.isFirstMeaningForLang(searchterm)) result1Commonness*=3
-                    if (result2.isFirstMeaningForLang(searchterm)) result2Commonness*=3
-                }
-                return result2Commonness - result1Commonness
-            }
-        })
+    //     // if (entries.length > 250) entries.subList(250, entries.length).clear();
+    //     if (entries.length > 250) entries.length = 250
 
-        // if (entries.length > 250) entries.subList(250, entries.length).clear();
-        if (entries.length > 250) entries.length = 250
+    //     if (printTime){
+    //         console.log('Query: ' +  process.hrtime(startTime)[1]/1000000 + ' ms.')
+    //     }
 
-        if (printTime){
-            console.log('Query: ' +  process.hrtime(startTime)[1]/1000000 + ' ms.')
-        }
+    //     return entries
 
-        return entries
-
-    })
+    // })
     
 
 }
@@ -395,40 +326,6 @@ function getEntryIds(hit_entseq, sqlquery, searchterm) {
     })
 
 }
-function addLanguageFilter(sqlquery, showGerman, showEnglish){
-    if (showEnglish && showGerman) return sqlquery
-
-    let langids = []
-    if(showEnglish)langids.push(langIdsMapping[Languages.eng])
-    if(showGerman)langids.push(langIdsMapping[Languages.ger])
-    sqlquery += ' and lang IN ('+ langids.join(',')+')'
-    return sqlquery
-
-}
-
-function getSuggestionsCursor(query, showGerman, showEnglish) {
-
-    let promises = []
-    if (containsKanji(query)){
-        let sqlquery = 'SELECT _id, kanji as Suggest FROM kanjis WHERE kanji LIKE \''+query+'%\' '
-        sqlquery = addLanguageFilter(sqlquery, showGerman, showEnglish)
-        sqlquery+='group by kanji Order by num_occurences desc LIMIT 10'
-        promises.push(db.allAsync(sqlquery))
-    }
-    if (containsKana(query)){
-        let sqlquery = 'SELECT _id, kana as Suggest FROM kanas WHERE kana LIKE \''+query+'%\' '
-        sqlquery = addLanguageFilter(sqlquery, showGerman, showEnglish)
-        sqlquery+='group by kana Order by num_occurences desc LIMIT 10'
-        promises.push(db.allAsync(sqlquery))
-    }
-    let sqlquery = 'SELECT _id, meaning as Suggest, count(meaning) as cnt FROM meanings WHERE meaning LIKE \''+query+'%\' '
-    sqlquery = addLanguageFilter(sqlquery, showGerman, showEnglish)
-    sqlquery+='group by meaning Order by cnt desc LIMIT 10'
-    promises.push(db.allAsync(sqlquery))
-    return Promise.all(promises)
-
-}
-
 function getSuggestions(query) {
     getSuggestionsCursor(query).then(function(rows){
         return rows
@@ -438,13 +335,13 @@ function getSuggestions(query) {
 
 function startSearch(){
     // findEntrys('Geisterhaus', true, true, true , true , true)
-    findEntrys('book', true, true, true , true , true).then(function(entries){
-        console.log(JSON.stringify(entries, null, 2))
-    })
+    // findEntrys('book', true, true, true , true , true).then(function(entries){
+    //     console.log(JSON.stringify(entries, null, 2))
+    // })
     // findEntrys('どうながたんそく', true, true, true , true , true)
     
 }
-startSearch()
+// startSearch()
 
 exports.handler = (event, context, callback) => {
 
